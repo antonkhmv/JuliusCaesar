c6756557b74050cee18bb1928808429c976f1ad2
Anton Khomyakov <antonkhmv@gmail.com>
2021-05-12 13:15:55 +0300

    Initial Commit

---
+++ DerivedData/JuliusCeasar/Build/Intermediates.noindex/JuliusCeasar.build/Debug/TdlibKit.build/DerivedSources/TdlibKit_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char TdlibKitVersionString[];
+ extern const double TdlibKitVersionNumber;
+
+ const unsigned char TdlibKitVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:TdlibKit  PROJECT:JuliusCeasar-1" "\n";
+ const double TdlibKitVersionNumber __attribute__ ((used)) = (double)1.;
